3197c4d876f316302cf5577405bcfb73da204159
Gustavo Salas <gustavosalas@Gustavos-Air.attlocal.net>
2023-09-15 20:06:51 -0700

    updated linkedin info

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -5,7 +5,7 @@ export default function Buttons() {
     return (
         <div className='btns-container'>
             <a href="mailto:someaddress@gmail.com" className='btn btn_email'><i class="fa-solid fa-envelope"></i> Email</a>
-            <a href="mailto:someaddress@gmail.com" className='btn btn_linkedin'><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
+            <a href="https://www.linkedin.com/in/gustavo-ruiz-salas-407891102/" className='btn btn_linkedin'><i class="fa-brands fa-linkedin"></i> LinkedIn</a>
         </div>
     )
 }
